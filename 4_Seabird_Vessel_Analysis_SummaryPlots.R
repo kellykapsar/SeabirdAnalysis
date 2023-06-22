a48b8494b5d2ba2c17d1245bd87dabf3bcbb2b9d
kellykapsar <kelly.kapsar@gmail.com>
2023-06-22 10:40:55 -0500

    Update summary plots to count pct risky cells and compare all traffic/night traffic by season.

--- 4_Seabird_Vessel_Analysis_SummaryPlots.R
+++ 4_Seabird_Vessel_Analysis_SummaryPlots.R
@@ -3,37 +3,52 @@
 library(sf)
 library(tidyverse)
 
+loc <- "Northern Bering"
 loc <- "Gulf of Alaska"
+loc <- "All Alaska"
+loc <- "Aleutian"
 
 
-files <- intersect(list.files("../Data_Processed/FinalShapefiles/", pattern=loc, full.names=T), 
-                   list.files("../Data_Processed/FinalShapefiles/", pattern=".shp", full.names = T))
+filelist <- list.files("../Data_Processed/AllVsNightRisk/", pattern=loc, full.names=T)
+
+files <- lapply(filelist, read.csv)
+hexes <- do.call(rbind, files)
+
+hexes$pct_hi_summ_all <- round(hexes$allhighrisk_Summer/hexes$numhexes*100,2)
+hexes$pct_hi_summ_night <- round(hexes$nighthighrisk_Summer/hexes$numhexes*100,2)
+
+hexes$pct_hi_fall_all <- round(hexes$allhighrisk_Fall/hexes$numhexes*100,2)
+hexes$pct_hi_fall_night <- round(hexes$nighthighrisk_Fall/hexes$numhexes*100,2)
+
+h <- hexes %>% select(pct_hi_summ_all, pct_hi_summ_night, pct_hi_fall_all, pct_hi_fall_night, taxa) %>% gather(key=subset,value=riskpct, -taxa)
+
+test <- strsplit(h$subset,split = "_")
+h$season <- sapply(test, "[[", 3)
+h$timeofday <-  sapply(test, "[[", 4)
+
+p <- ggplot(h, aes(x=season, y=riskpct, fill=timeofday)) +
+  geom_bar(position="dodge", stat="identity") +
+  scale_fill_manual(values=c("#fee227", "#191970")) +
+  theme(axis.text.x = element_text(angle=45, hjust=1)) +
+  facet_wrap(~taxa)
+  # annotate("text", x = 1:4, y = - 400,
+  #          label = rep(c("Variety 1", "Variety 2"), 2)) +
+  # annotate("text", c(1.5, 3.5), y = -0.24, label = c("Irrigated", "Dry")) +
+  # theme_classic() +
+  # theme(plot.margin = unit(c(1, 1, 4, 1), "lines"),
+  #       axis.title.x = element_blank(),
+  #       axis.text.x = element_blank())
+p
+ggsave(filename = "../Figures/RiskBarGraph.png",
+       plot = p, width=16, height=8, units="in")
 
 
+##################
+# Log bird density vs. log vessel traffic - Risk Categories
+akall <- st_read("../Data_Processed/FinalShapefiles/AllSeasonsAllTimeOfDay_All Alaska_Seabirds.shp") %>% st_drop_geometry()
+akallsumm <- akall %>% filter(subset == "Summer_All")
+akallfall <- akall %>% filter(subset == "Fall_All")
 
-
-df <- data.frame(do.call(rbind, strsplit(files, "_"))) %>% select(-X1, -X2)
-colnames(df) <- c("location", "taxa", "season")
-df$season <- factor(df$season, levels=c("Summer", "Fall"))
-df$nhex <- NA
-df$riskpct <- NA
-df$risk <- NA
-df$den <- NA
-df$denpct <- NA
-
-for(i in 1:length(files)){
-  t <- st_read(files[i], quiet=T) %>% st_drop_geometry()
-  df$nhex[i] <- length(t$hexID)
-  df$risk[i] <- sum(t$risk %in% c("high", "veryhigh"))
-  df$riskpct[i] <- round(df$risk[i]/df$nhex[i]*100, 2)
-  df$den[i] <- length(which(t$ClassBird == 3))
-  df$denpct[i] <- round(df$den[i]/df$nhex[i]*100, 2)
-}
-
-akallsumm <- st_read("../Data_Processed/FinalDF_All Alaska_Seabirds_Summer_NightOnlyFALSE.shp") %>% st_drop_geometry()
-akallfall <- st_read("../Data_Processed/FinalDF_All Alaska_Seabirds_Fall_NightOnlyFALSE.shp") %>% st_drop_geometry()
-
-# Log bird density vs. log vessel traffic
 akallsumm$risk <- factor(akallsumm$risk, levels = c("low", "medium", "high", "veryhigh"), labels = c("Low", "Medium", "High", "Very High"))
 akallfall$risk <- factor(akallfall$risk, levels = c("low", "medium", "high", "veryhigh"), labels = c("Low", "Medium", "High", "Very High"))
 
@@ -53,26 +68,4 @@ ggplot(akallfall, aes(x = DensBird, y = AllShip)) +
   scale_x_continuous(trans=scales::pseudo_log_trans(base = 10)) +
   scale_y_continuous(trans=scales::pseudo_log_trans(base = 10)) +
   theme(text = element_text(size=30))
-
-######
-
-ggplot(df, aes(x = denpct, y = riskpct)) +
-  geom_point(aes(color=season)) +
-  theme(text = element_text(size=30)) 
-
-ggplot(df, aes(x = denpct, y = riskpct)) +
-  geom_point(aes(color=season)) +
-  theme(text = element_text(size=30)) +
-  stat_smooth(method="lm", se=TRUE)  +
-  geom_abline(color="black", lwd=1)
-
-dfnew <- df %>% filter(taxa %in% c("Albatross", "Shearwaters", "Auklets", "Murres", "Kittiwakes", "Gulls", "Storm Petrels", "Northern Fulmars"))
-
-p <- ggplot(df, aes(x=reorder(taxa, -riskpct), y=riskpct, fill=season)) +
-  geom_bar(position="dodge", stat="identity") +
-  scale_fill_manual(values=c("#7ab845", "#44828f")) +
-  theme(axis.text.x = element_text(angle=45, hjust=1))
-
-ggsave(filename = "../Figures/RiskBarGraph.png",
-       plot = p, width=16, height=8, units="in")
 
