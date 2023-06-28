a4801a9c82a159c9e08b6dc6003772a192d00419
kellykapsar <kelly.kapsar@gmail.com>
2023-06-28 16:53:16 -0500

    Plot for percentage of high and very high risk hexes by region.

--- 4_Seabird_Vessel_Analysis_SummaryPlots.R
+++ 4_Seabird_Vessel_Analysis_SummaryPlots.R
@@ -3,44 +3,205 @@
 library(sf)
 library(tidyverse)
 
-loc <- "Northern Bering"
-loc <- "Gulf of Alaska"
-loc <- "All Alaska"
-loc <- "Aleutian"
-
-
-filelist <- list.files("../Data_Processed/AllVsNightRisk/", pattern=loc, full.names=T)
+# Master summary statistics csv file 
+filelist <- list.files("../Data_Processed/SummaryStatistics/", pattern=".csv", full.names=T)
 
 files <- lapply(filelist, read.csv)
-hexes <- do.call(rbind, files)
+summstats <- do.call(rbind, files)
+write.csv(summstats, "../Data_Processed/Master_SummaryStatistics.csv")
+rm(filelist)
+rm(files)
 
-hexes$pct_hi_summ_all <- round(hexes$allhighrisk_Summer/hexes$numhexes*100,2)
-hexes$pct_hi_summ_night <- round(hexes$nighthighrisk_Summer/hexes$numhexes*100,2)
+# Load in basemap for plots
+basemap <- st_read("../Data_Raw/AK_CAN_RUS/AK_CAN_RUS.shp") 
 
-hexes$pct_hi_fall_all <- round(hexes$allhighrisk_Fall/hexes$numhexes*100,2)
-hexes$pct_hi_fall_night <- round(hexes$nighthighrisk_Fall/hexes$numhexes*100,2)
+hexMask <- st_read("../Data_Raw/hex_x_ocean/hex_x_ocean.shp") %>%
+  select(hexID) %>%
+  mutate(AreaKM = c(st_area(.)/1000000)) 
 
-h <- hexes %>% select(pct_hi_summ_all, pct_hi_summ_night, pct_hi_fall_all, pct_hi_fall_night, taxa) %>% gather(key=subset,value=riskpct, -taxa)
+# List of regions 
+loclist <- c("Northern Bering & Chukchi Seas", "Gulf of Alaska", "All Alaska", "Eastern Aleutians")
 
-test <- strsplit(h$subset,split = "_")
-h$season <- sapply(test, "[[", 3)
-h$timeofday <-  sapply(test, "[[", 4)
+for(i in 1:length(loclist)){
 
-p <- ggplot(h, aes(x=season, y=riskpct, fill=timeofday)) +
-  geom_bar(position="dodge", stat="identity") +
-  scale_fill_manual(values=c("#fee227", "#191970")) +
-  theme(axis.text.x = element_text(angle=45, hjust=1)) +
-  facet_wrap(~taxa)
-  # annotate("text", x = 1:4, y = - 400,
-  #          label = rep(c("Variety 1", "Variety 2"), 2)) +
-  # annotate("text", c(1.5, 3.5), y = -0.24, label = c("Irrigated", "Dry")) +
-  # theme_classic() +
-  # theme(plot.margin = unit(c(1, 1, 4, 1), "lines"),
-  #       axis.title.x = element_blank(),
-  #       axis.text.x = element_blank())
-p
-ggsave(filename = "../Figures/RiskBarGraph.png",
-       plot = p, width=16, height=8, units="in")
+  filelist <- list.files("../Data_Processed/AllVsNightRisk/", pattern=loclist[i], full.names=T)
+  
+  files <- lapply(filelist, read.csv)
+  hexes <- do.call(rbind, files)
+  
+  
+  hexes$pct_hi_summ_all <- round(hexes$allhighrisk_Summer/hexes$numhexes*100,2)
+  hexes$pct_hi_summ_night <- round(hexes$nighthighrisk_Summer/hexes$numhexes*100,2)
+  
+  hexes$pct_hi_fall_all <- round(hexes$allhighrisk_Fall/hexes$numhexes*100,2)
+  hexes$pct_hi_fall_night <- round(hexes$nighthighrisk_Fall/hexes$numhexes*100,2)
+  
+  h <- hexes %>% select(pct_hi_summ_all, pct_hi_summ_night, pct_hi_fall_all, pct_hi_fall_night, taxa) %>% gather(key=subset,value=riskpct, -taxa)
+  
+  test <- strsplit(h$subset,split = "_")
+  h$season <- sapply(test, "[[", 3)
+  h$timeofday <-  sapply(test, "[[", 4)
+  
+  h$taxa <- factor(h$taxa, 
+                   levels = c("Seabirds", "Auklets", "Northern Fulmars", "Seaducks", "Shearwaters", "Storm Petrels"),
+                   labels = c("All Seabirds", "Auklets", "Northern Fulmars", "Seaducks", "Shearwaters", "Storm Petrels"))
+  h$season <- factor(h$season, levels = c("summ", "fall"), labels = c("Summer", "Fall"))
+  
+  p <- ggplot(h, aes(x=season, y=riskpct, fill=timeofday)) +
+    geom_bar(position="dodge", stat="identity") +
+    scale_fill_manual(values=c("all" = "#fee227", 
+                               "night" = "#191970"), 
+                      labels = c("All", "Night Only"),
+                      name="Vessel Traffic") +
+    theme(axis.text.x = element_text(angle=45, hjust=1)) +
+    facet_wrap(~taxa) + 
+    ylab("Percent of Study Area at High or Very High Risk") + 
+    xlab("") +
+    theme_bw() + 
+    theme(text = element_text(size=20))
+  p
+  ggsave(filename = paste0("../Figures/RiskBarGraph_", loclist[i],".png"),
+         plot = p, width=16, height=8, units="in")
+  ############################################
+  
+  filelist <- list.files("../Data_Processed/FinalShapefiles/", pattern=loclist[i], full.names=T)
+  filelist <- filelist[grep(".shp", filelist)]
+  filelist <- filelist[-grep("Seabird", filelist)]
+  
+  
+  files <- lapply(filelist, st_read)
+  hexes <- do.call(rbind, files)
+  
+  test <- hexes %>% st_drop_geometry() %>% filter(risk %in% c("high", "veryhigh")) %>% group_by(hexID, season, timeofday) %>% summarize(n=length(unique(taxa)))
+  
+  hexMaskCrop <- hexMask[hexMask$hexID %in% unique(hexes$hexID),]
+  
+  combos <- data.frame(id = 1:4, 
+                       season = c("Summer", "Fall", "Summer", "Fall"), 
+                       tod = c("All", "All", "Night", "Night"))
+  
+  basemapnew <- basemap %>% st_crop(st_buffer(st_as_sfc(st_bbox(hexMaskCrop)), 10000))
+
+  box <- st_as_sf(name="boundary", st_buffer(st_as_sfc(st_bbox(hexMaskCrop)), 10000))
+  
+  ################################################# 
+  ##################### Risk plot: All Traffic #################
+  ################################################# 
+  taxariskcounts <- paste0("../Figures/RiskCounts_All_", loclist[i], ".png")
+  
+  for(j in 1:2){
+    t <- test %>% filter(season == combos$season[j] & timeofday == combos$tod[j])
+    t$n <- factor(t$n)
+    tnew <- left_join(hexMaskCrop, t, by=c("hexID"))
+    
+    plt <- ggplot() +
+      geom_sf(data=box, fill=NA, color=NA,lwd=0) +      
+      geom_sf(data=basemapnew, fill="lightgray",lwd=0) +
+      geom_sf(data=tnew,aes(fill = n), color="darkgray") +
+      scale_fill_manual(values = c("1" = "#ec8282",
+                                   "2" = "#c37b87",
+                                   "3" = "#826f8d",
+                                   "4" = "#446382"),
+                        na.value = "white",
+                        name="Number of Taxa", 
+                        drop=F) +
+      xlab("") +
+      ylab("") +
+      scale_x_continuous(expand = c(0, 0)) +
+      scale_y_continuous(expand = c(0, 0)) +
+      theme_bw() +
+      theme(text = element_text(size = 18),
+            plot.title = element_text(hjust = 0.5),
+            plot.subtitle = element_text(hjust = 0.5),
+            plot.caption = element_text(size = 8, hjust=0),
+            axis.ticks = element_blank(),
+            axis.text=element_blank(),
+            panel.border =  element_rect(colour = "black"),
+            panel.grid.major = element_line(colour = "transparent"), 
+            panel.background = element_rect(fill = "white")) +
+      ggtitle(paste0(t$season[1]))
+    assign(paste0("p", j), plt)
+  }
+  comboplot <- ggarrange(p1, p2, ncol=2, nrow=1, common.legend=TRUE, legend = "bottom")
+  comboplot <- annotate_figure(comboplot, top = text_grob(tnew$taxa[1], face = "bold", size = 30)) + 
+    theme(panel.background = element_rect(fill = "white"))
+  
+  # Save figure
+  if(!file.exists(taxariskcounts)){
+    ifelse(loclist[i] == "Eastern Aleutians", 
+           ggsave(filename=taxariskcounts, 
+                  plot= comboplot, 
+                  width=12, height=4, units="in"),
+           ggsave(filename=taxariskcounts, 
+                  plot= comboplot, 
+                  width=12, height=6, units="in"))
+  }
+  
+  ################################################# 
+  ##################### Risk plot: Night Traffic #################
+  ################################################# 
+  taxanightriskcounts <- paste0("../Figures/RiskCounts_Night_", loclist[i], ".png")
+  
+  for(k in 3:4){
+    t <- test %>% filter(season == combos$season[k] & timeofday == combos$tod[k])
+    t$n <- factor(t$n)
+    tnew <- left_join(hexMaskCrop, t, by=c("hexID"))
+    
+    plt <- ggplot() +
+      geom_sf(data=box, fill=NA, color=NA,lwd=0) +      
+      geom_sf(data=basemapnew, fill="lightgray",lwd=0) +
+      geom_sf(data=tnew,aes(fill = n), color="darkgray") +
+      scale_fill_manual(values = c("1" = "#ec8282",
+                                   "2" = "#c37b87",
+                                   "3" = "#826f8d",
+                                   "4" = "#446382"),
+                        na.value = "white",
+                        name="Number of Taxa", 
+                        drop=F) +
+      xlab("") +
+      ylab("") +
+      scale_x_continuous(expand = c(0, 0)) +
+      scale_y_continuous(expand = c(0, 0)) +
+      theme_bw() +
+      theme(text = element_text(size = 18),
+            plot.title = element_text(hjust = 0.5),
+            plot.subtitle = element_text(hjust = 0.5),
+            plot.caption = element_text(size = 8, hjust=0),
+            axis.ticks = element_blank(),
+            axis.text=element_blank(),
+            panel.border =  element_rect(colour = "black"),
+            panel.grid.major = element_line(colour = "transparent"), 
+            panel.background = element_rect(fill = "white")) +
+      ggtitle(paste0(t$season[1]))
+    assign(paste0("p", k), plt)
+  }
+  comboplot <- ggarrange(p1, p2, ncol=2, nrow=1, common.legend=TRUE, legend = "bottom")
+  comboplot <- annotate_figure(comboplot, top = text_grob(tnew$taxa[1], face = "bold", size = 30)) + 
+    theme(panel.background = element_rect(fill = "white"))
+  
+  # Save figure
+  if(!file.exists(taxariskcounts)){
+    ifelse(loclist[i] == "Eastern Aleutians", 
+           ggsave(filename=taxariskcounts, 
+                  plot= comboplot, 
+                  width=12, height=4, units="in"),
+           ggsave(filename=taxariskcounts, 
+                  plot= comboplot, 
+                  width=12, height=6, units="in"))
+  }
+  
+}
+
+# hall <- h %>% filter(timeofday == "all")
+# 
+# p <- ggplot(hall, aes(x=taxa, y=riskpct, fill=season)) +
+#   geom_bar(position="dodge", stat="identity") +
+#   scale_fill_manual(values=c("#fee227", "#191970")) +
+#   theme(axis.text.x = element_text(angle=45, hjust=1)) 
+# p
+# ggsave(filename = "../Figures/RiskBarGraph.png",
+#        plot = p, width=16, height=8, units="in")
+
 
 
 ##################
